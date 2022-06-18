8932d478028d101630caf825f5c7410de0d56584
NurRozaqiUmam <107600122+NurRozaqiUmam@users.noreply.github.com>
2022-06-18 14:45:44 +0700

    kategpri produk
    membuat dan me link kategori produk

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -55,7 +55,13 @@
             </div>
             <div class="form-group">
                 <label>Kategori</label>
-                <input type="text" name="kategori" class="form-control">
+                <select class="form-control" name="kategori">
+                <option>Elektronik</option>
+                <option>Pakaian Pria</option>
+                <option>Pakaian Wanita</option>
+                <option>Pakaian Anak Anak</option>
+                <option>Peralatan Olahraga</option>
+                </select>
             </div>
             <div class="form-group">
                 <label>Harga</label>
