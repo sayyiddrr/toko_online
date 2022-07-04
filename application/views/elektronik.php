c51acb6c2591aac1dbcad43d59ba705bae31a326
NurRozaqiUmam <107600122+NurRozaqiUmam@users.noreply.github.com>
2022-07-04 23:33:16 +0700

    update categories
    update categories dan me link setiap page

--- application/views/elektronik.php
+++ application/views/elektronik.php
@@ -1,42 +1,316 @@
-<div class="container-fluid">
-    
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="<?php echo base_url('assets/img/slider1.jpg') ?>" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo base_url('assets/img/slider2.jpg') ?>" alt="Second slide">
-            </div>
-            
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <div class="row text-center mt-4">
+<!-- Start Banner Area -->
+<section class="banner-area organic-breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+			<div class="col-first">
+				<h1>Shop Category</h1>
+				<nav class="d-flex align-items-center">
+					<a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
+					<a href="#">Shop<span class="lnr lnr-arrow-right"></span></a>
+					<a href="category.html">Shop Category</a>
+				</nav>
+			</div>
+		</div>
+	</div>
+</section>
+<br>
+<!-- End Banner Area -->
+<div class="container">
+	<div class="row">
+		<div class="col-xl-3 col-lg-4 col-md-5">
+			<div class="sidebar-categories">
+				<div class="head">Browse Categories</div>
+				<ul class="main-categories">
+					<li class="main-nav-list">
+						<a href="<?php echo base_url('categories/elektronik') ?>" aria-expanded="false" aria-controls="elektronik">Elektronik</a>
+					</li>
 
-        <?php foreach ($elektronik as $brg) : ?>
-            <div class="card ml-3 mb-3" style="width: 16rem;">
-                <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
-                    <small><?php echo $brg->keterangan ?></small><br>
-                    <span class="badge badge-pill badge-danger mb-2">Rp. <?php echo number_format($brg->harga, 0,',','.') ?></span><br>
-                    <?php echo anchor('dashboard/tambah_ke_keranjang/'.$brg->id_brg,'<div class="btn btn-sm btn-primary">Add to Chart</div>') ?>
-                    <?php echo anchor('dashboard/detail/'.$brg->id_brg,'<div class="btn btn-sm btn-success">Detail</div>') ?>
-                </div>
-            </div>
-                        
-        <?php endforeach; ?>
-    </div>
+					<li class="main-nav-list">
+						<a href="<?php echo base_url('categories/pakaian_pria') ?>" aria-expanded="false" aria-controls="pakaianpria">Pakaian Pria</a>
+					</li>
+
+					<li class="main-nav-list">
+						<a href="<?php echo base_url('categories/pakaian_wanita') ?>" aria-expanded="false" aria-controls="pakaianwanita">Pakaian Wanita</a>
+					</li>
+
+					<li class="main-nav-list">
+						<a href="<?php echo base_url('categories/pakaian_anak_anak') ?>" aria-expanded="false" aria-controls="pakaiananakanak">Pakaian Anak-Anak</a>
+					</li>
+
+					<li class="main-nav-list">
+						<a href="<?php echo base_url('categories/peralatan_olahraga') ?>" aria-expanded="false" aria-controls="peralatanolahraga">Peralatan Olahraga</a>
+					</li>
+
+				</ul>
+			</div>
+			<div class="sidebar-filter mt-50">
+				<div class="top-filter-head">Product Filters</div>
+				<div class="common-filter">
+					<div class="head">Brands</div>
+					<form action="#">
+						<ul>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="apple" name="brand"><label for="apple">Apple<span>(29)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="asus" name="brand"><label for="asus">Asus<span>(29)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="gionee" name="brand"><label for="gionee">Gionee<span>(19)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="micromax" name="brand"><label for="micromax">Micromax<span>(19)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="samsung" name="brand"><label for="samsung">Samsung<span>(19)</span></label></li>
+						</ul>
+					</form>
+				</div>
+				<div class="common-filter">
+					<div class="head">Color</div>
+					<form action="#">
+						<ul>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="black" name="color"><label for="black">Black<span>(29)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="balckleather" name="color"><label for="balckleather">Black
+									Leather<span>(29)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="blackred" name="color"><label for="blackred">Black
+									with red<span>(19)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="gold" name="color"><label for="gold">Gold<span>(19)</span></label></li>
+							<li class="filter-list"><input class="pixel-radio" type="radio" id="spacegrey" name="color"><label for="spacegrey">Spacegrey<span>(19)</span></label></li>
+						</ul>
+					</form>
+				</div>
+				<div class="common-filter">
+					<div class="head">Price</div>
+					<div class="price-range-area">
+						<div id="price-range"></div>
+						<div class="value-wrapper d-flex">
+							<div class="price">Price:</div>
+							<span>$</span>
+							<div id="lower-value"></div>
+							<div class="to">to</div>
+							<span>$</span>
+							<div id="upper-value"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xl-9 col-lg-8 col-md-7">
+			<!-- Start Filter Bar -->
+			<div class="filter-bar d-flex flex-wrap align-items-center">
+				<div class="sorting">
+					<select>
+						<option value="1">Default sorting</option>
+						<option value="1">Default sorting</option>
+						<option value="1">Default sorting</option>
+					</select>
+				</div>
+				<div class="sorting mr-auto">
+					<select>
+						<option value="1">Show 12</option>
+						<option value="1">Show 12</option>
+						<option value="1">Show 12</option>
+					</select>
+				</div>
+				<div class="pagination">
+					<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+					<a href="#" class="active">1</a>
+					<a href="#">2</a>
+					<a href="#">3</a>
+					<a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+					<a href="#">6</a>
+					<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+				</div>
+			</div>
+			<!-- End Filter Bar -->
+			<!-- Start Best Seller -->
+			<section class="lattest-product-area pb-40 category-list">
+				<div class="row">
+					<?php foreach ($elektronik as $brg) : ?>
+					<!-- single product -->
+						<div class="col-lg-4 col-md-6">
+							<div class="single-product">
+								<img class="img-fluid" src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" alt="">
+								<div class="product-details">
+									<h6><?php echo $brg->nama_brg ?></h6>
+									<div class="price">
+										<h6>Rp. <?php echo number_format($brg->harga, 0,',','.') ?></h6>
+									</div>
+									<div class="prd-bottom">
+
+										<a href="<?php echo base_url('categories/tambah_ke_keranjang/'.$brg->id_brg )?>" class="social-info">
+											<span class="ti-bag"></span>
+											<p class="hover-text">add to bag</p>
+										</a>
+										<a href="" class="social-info">
+											<span class="lnr lnr-heart"></span>
+											<p class="hover-text">Wishlist</p>
+										</a>
+										<a href="" class="social-info">
+											<span class="lnr lnr-sync"></span>
+											<p class="hover-text">compare</p>
+										</a>
+										<a href="" class="social-info">
+											<span class="lnr lnr-move"></span>
+											<p class="hover-text">view more</p>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</section>
+			<!-- End Best Seller -->
+			<!-- Start Filter Bar -->
+			<div class="filter-bar d-flex flex-wrap align-items-center">
+				<div class="sorting mr-auto">
+					<select>
+						<option value="1">Show 12</option>
+						<option value="1">Show 12</option>
+						<option value="1">Show 12</option>
+					</select>
+				</div>
+				<div class="pagination">
+					<a href="#" class="prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a>
+					<a href="#" class="active">1</a>
+					<a href="#">2</a>
+					<a href="#">3</a>
+					<a href="#" class="dot-dot"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+					<a href="#">6</a>
+					<a href="#" class="next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+				</div>
+			</div>
+			<!-- End Filter Bar -->
+		</div>
+	</div>
 </div>
+
+<!-- Start related-product Area -->
+<section class="related-product-area section_gap">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-6 text-center">
+				<div class="section-title">
+					<h1>Deals of the Week</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-9">
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r1.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r2.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r3.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r5.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r6.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r7.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r9.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r10.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="single-related-product d-flex">
+							<a href="#"><img src="img/r11.jpg" alt=""></a>
+							<div class="desc">
+								<a href="#" class="title">Black lace Heels</a>
+								<div class="price">
+									<h6>$189.00</h6>
+									<h6 class="l-through">$210.00</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="ctg-right">
+					<a href="#" target="_blank">
+						<img class="img-fluid d-block mx-auto" src="img/category/c5.jpg" alt="">
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End related-product Area -->
