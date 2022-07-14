92f6ef00392cf1e115952c34d5759c3473e57423
NazriTantowi <muhammadnazritantowi@if.uai.ac.id>
2022-07-14 09:52:00 +0700

    aaagh

---
+++ application/views/admin/metode.php
@@ -0,0 +1,52 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Payment Configuration
+            <small>Sazan Shop</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Metode</li>
+        </ol>
+    </section>
+    <section class="content">
+        <h4>List Metode Order</h4>
+
+        <table class="table table-bordered table-hover table-striped">
+
+            <tr>
+                <th>PEMBAYARAN</th>
+                <th>PENGIRIMAN</th>
+            </tr>
+
+            <tr>
+                <td>BCA</td>
+                <td>POS</td>
+            </tr>
+            <tr>
+                <td>BNI</td>
+                <td>JNE</td>
+            </tr>
+            <tr>
+                <td>BRI</td>
+                <td>JNT</td>
+            </tr>
+            <tr>
+                <td>Mandiri</td>
+                <td>TIKI</td>
+            </tr>
+                <td>SazanPay</td>
+            </tr>
+            
+
+        </table>
+
+        <a href="<?php echo base_url('admin/invoice/index')?>"><div class="btn btn-sm btn-primary">kembali</div></a>
+    </section>
+</div>
+<!-- /.content-wrapper -->
+
+</body>
+</html>
