5f9323f4d2ad14d05f3ac841f69175612031ff17
NazriTantowi <muhammadnazritantowi@if.uai.ac.id>
2022-07-20 22:49:16 +0700

    BULAN

--- application/controllers/admin/chart.php
+++ application/controllers/admin/chart.php
@@ -4,6 +4,7 @@ class Chart extends CI_Controller{
     public function index()
     {
         $data['admin'] = $this->db->get_where('tb_admin', ['adminID' => $this->session->userdata('adminID')])->row_array();
+        
         $this->load->view('templates_admin/header', $data);
         $this->load->view('templates_admin/sidebar', $data);
         $this->load->view('admin/chart', $data);
